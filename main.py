ecf49f92cec4061827ed67f8fc99434b83cbdbf1
Evans Murunga <91890645+Fffbbm@users.noreply.github.com>
2023-05-27 21:21:54 +0300

    data savedata = new

---
+++ main.py
@@ -0,0 +1,22 @@
+import json
+import hashlib
+print("Validate Data")
+username = input("Enter your username: ")
+new_password = input("Enter your password: ")
+
+n_pd = hashlib.md5(new_password.encode()).hexdigest()
+with open("data.json", "r") as f:
+    data1 = json.load(f)
+
+# Search for matching credentials in the data
+matching_section = None
+for section in data1:
+    if section["username"] == username and section["password"] == n_pd:
+        matching_section = section
+        break
+
+if matching_section:
+    matching_json = json.dumps(matching_section, indent=2)
+    print(matching_json)
+else:
+    print("Invalid username or password")
